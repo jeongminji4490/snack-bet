d9cb319eafa1d966ca898703af8c4a3efbc58315
gea-minji <minji.jeong@geappliances.com>
2023-03-08 20:33:57 +0900

    Initial Commit

---
+++ snack/snackApp.swift
@@ -0,0 +1,17 @@
+//
+//  snackApp.swift
+//  snack
+//
+//  Created by Jeong Minji on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct snackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
